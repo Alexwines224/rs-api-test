956b3e5cc30235b7e0bec3d9f9da2bb14481480f
Alex Wines <alexwines224@yahoo.com>
2016-03-21 15:26:52 -0700

    first commit

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,36 @@
+module API
+
+	class UsersController < ApplicationController
+		
+		protect_from_forgery with: :null_session
+		respond_to :json
+
+		def index
+			users = User.all 
+			respond_with users
+		end
+
+		def show
+			user = User.find(params[:id])
+			respond_with user
+		end
+
+		def create
+			user = User.new(user_params)
+			if user.save
+				respond_with user, location: [:api, user]
+			else
+			  respond_with user
+			end
+		end
+
+		private
+
+		def user_params
+			params.require(:user).permit(:phone_number, :message])
+		end
+
+
+	end
+
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    phone_number "MyString"
+    message "MyString"
+  end
+end
